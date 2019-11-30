e6d9a52c9a4d754ad9c3e2c8099b80a247a4161a
Jorian Baeke <jorian@rhinox.training>
2019-11-30 15:06:20 +0100

    Initial setup sound

---
+++ HotSkater/Assets/SoundManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Rhinox.Utilities;
+using UnityEngine;
+
+public class SoundManager : Singleton<SoundManager>
+{
+    public AudioInstrument[] Instruments;
+
+    private List<InstrumentAudio> _activeLoops;
+
+    private void Start()
+    {
+        _activeLoops = new List<InstrumentAudio>();
+    }
+
+    public void StartLoop(AudioInstrument instrument, int index)
+    {
+        var loop = new InstrumentAudio
+        {
+            Instrument = instrument,
+            LoopIndex = index
+        };
+        _activeLoops.Add(loop);
+        
+        loop.Start();
+    }
+}
