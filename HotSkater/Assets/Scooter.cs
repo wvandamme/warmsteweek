efa4dee400f412baa73f9c05a2a9be1769b44859
Jorian Baeke <jorian@rhinox.training>
2019-11-30 19:47:33 +0100

    calibration

--- HotSkater/Assets/Scooter.cs
+++ HotSkater/Assets/Scooter.cs
@@ -8,6 +8,20 @@ public class Scooter : MonoBehaviour
     public bool ExecuteInEditMode = true;
     public Transform FollowObject;
 
+    private Vector3 _prevPosition;
+    private Vector3 _targetPosition;
+    private Vector3 _prevForward;
+    private Vector3 _targetForward;
+
+    public float MoveSpeed = 1f;
+    public float RotationSpeed = 45f;
+
+    public void Start()
+    {
+        _targetPosition = transform.position;
+        _targetForward = transform.forward;
+    }
+    
     public void Update()
     {
         if (FollowObject != null)
@@ -18,7 +32,26 @@ public class Scooter : MonoBehaviour
     {
         if (!Application.isPlaying && !ExecuteInEditMode)
             return;
-        
-        
+
+        var position = FollowObject.position;
+        position.y = 3f;
+        var forward = (position - _prevPosition).normalized;
+
+        var layerMask = LayerMask.GetMask("Ramp");
+        var dir = -Vector3.up;
+        var ray = new Ray(position, dir);
+        if (Physics.Raycast(ray, out RaycastHit hit, layerMask))
+        {
+            _targetPosition = hit.point;
+            if ((position - _prevPosition).magnitude > float.Epsilon * 50)
+                _targetForward = forward;
+
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, _targetPosition, Time.deltaTime * MoveSpeed);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(_targetForward), Time.deltaTime * RotationSpeed);
+
+        _prevPosition = position;
+        _prevForward = forward;
     }
 }
---
+++ HotSkater/Assets/Scripts/CalibrationManager.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class CalibrationManager : MonoBehaviour
+{
+    private const string CalibrationFile = "Calibration";
+    private string CalibrationFilePath => Path.Combine(Application.dataPath, CalibrationFile);
+    [ShowInInspector, ReadOnly] public bool Active { get; private set; }
+
+    public List<Vector3> CalibrationPoints;
+
+    public bool CalibrationNeeded => CalibrationPoints == null || CalibrationPoints.Count == 0;
+    
+    void Start()
+    {
+        CalibrationPoints = new List<Vector3>();
+        LoadCalibration();
+        
+        if (CalibrationNeeded && !Active)
+            ToggleActive();
+    }
+
+    void LoadCalibration()
+    {
+        if (!File.Exists(CalibrationFilePath)) return;
+        
+        var pointLines = File.ReadAllLines(CalibrationFilePath);
+        foreach (var line in pointLines)
+        {
+            var numsStr = line.Split(new[] { ';' });
+            if (numsStr.Length != 3)
+            {
+                Debug.LogError($"Invalid Point: '{line}'");
+                continue;
+            }
+
+            var nums = numsStr.Select( float.Parse ).ToArray();
+            var vec = new Vector3(nums[0], nums[1], nums[2]);
+            CalibrationPoints.Add(vec);
+        }
+    }
+
+    void SaveCalibration()
+    {
+        var lines = new List<string>();
+        foreach (var point in CalibrationPoints)
+        {
+            var text = $"{point.x};{point.y};{point.z}";
+            lines.Add(text);
+        }
+        
+        File.WriteAllLines(CalibrationFilePath, lines);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C) && Input.GetKey(KeyCode.LeftShift))
+            ToggleActive();
+        
+        if (CalibrationPoints == null || !CalibrationPoints.Any())
+        {
+            Debug.LogError("CalibrationNeeded");
+        }
+
+        if (!Active) return;
+            
+    }
+
+    private void ToggleActive()
+    {
+        if (CalibrationNeeded && Active)
+            return;
+        
+        Active = !Active;
+
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(Active);
+        }
+    }
+    
+    void OnGUI()
+    {
+        if (!Active) return;
+
+        if (CalibrationNeeded)
+        {
+            var oldColor = GUI.color;
+            GUI.color = Color.red;
+            GUILayout.Space(10);
+            GUILayout.Label("CALIBRATION NEEDED");
+            GUI.color = oldColor;
+        }
+        
+        GUILayout.Space(20);
+        
+        
+    }
+}
