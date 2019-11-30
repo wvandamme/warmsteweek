3a7555fbff7bf8217675cda5760bdff258c91009
Jorian Baeke <jorian@rhinox.training>
2019-11-30 17:55:07 +0100

    steam VR & instrument icons

---
+++ HotSkater/Assets/Scooter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Scooter : MonoBehaviour
+{
+    public bool ExecuteInEditMode = true;
+    public Transform FollowObject;
+
+    public void Update()
+    {
+        if (FollowObject != null)
+            HandleFollowObject();
+    }
+
+    private void HandleFollowObject()
+    {
+        if (!Application.isPlaying && !ExecuteInEditMode)
+            return;
+        
+        
+    }
+}
